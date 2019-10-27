a3c9df5aeb6f3f22a0a731a013773fe6a087fcf4
Eric_Lian <git@SuperExerciseBook.com>
2019-10-27 09:33:30 +0800

    完成渲染类测试

--- js/Game/GameDisplay.js
+++ js/Game/GameDisplay.js
@@ -41,6 +41,75 @@ class GameDisplay extends Display{
             CheckerBoard.innerHTML=content;
         }
 
+        /**
+         * 事件 游戏开始
+         * 
+         * 在游戏开始的时候本事件会被触发
+         * 
+         * 可在此处编写显示初始化需要执行的内容
+         * 
+         * @param {Event} e 
+         */
+        this.Event_GameStart=function(e){
+            console.log(e);
+            let Sign = document.getElementById('ELEM_Sign');
+            Sign.innerHTML="游戏开始";
+        }
+
+        /**
+         * 事件 游戏回合
+         * 
+         * 在游戏轮到新的玩家下棋的时候本事件会被触发
+         * 
+         * 可以在此处添加渲染提示标语的代码
+         * 
+         * @param {Event} e 
+         */
+        this.Event_Round=function(e){
+            console.log(e);
+            let Sign = document.getElementById('ELEM_Sign');
+            let s = "";
+
+            if (e.Operator == e.GameControl.Players[0]) {
+                s = "1P 黑方回合";
+            } else if (e.Operator == e.GameControl.Players[1]) {
+                s = "2P 白方回合";
+            } else {
+                throw new Error("WDNMD.");
+            }
+
+            Sign.innerHTML=s;
+        }
+        
+        /**
+         * 事件 游戏结束
+         * 
+         * 在游戏结束的时候本事件会被触发
+         * 
+         * @param {Event} e 
+         */
+        this.Event_GameEnd=function(e){
+            console.log(e);
+            let Sign = document.getElementById('ELEM_Sign');
+
+            // e.Winner.Scores[] // 得分
+            let s = ""+e.Winner.Scores[0]+":"+e.Winner.Scores[1]+" ";
+
+            if (e.Winner == e.GameControl.Players[0]) {
+                //1P 黑方胜
+                s=s+"1P 黑方胜";
+            } else if (e.Winner == e.GameControl.Players[2]) {
+                //2P 白方胜
+                s=s+"2P 白方胜";
+            } else if (e.WInner == e.GameControl) {
+                //平局
+                s=s+"平局";
+            }
+
+            Sign.innerHTML=s;
+            
+
+        }
 
     }
 
--- js/Game/Players/HumanPlayer.js
+++ js/Game/Players/HumanPlayer.js
@@ -8,8 +8,55 @@ class HumanPlayer extends Player{
      */
     constructor(){
         super();
+
+
+        /**
+         * 事件 游戏开始
+         * 
+         * 在游戏开始的时候本事件会被触发
+         * 
+         * 可在此处编写玩家初始化需要执行的内容
+         * 
+         * @param {Event} e 
+         */
+        this.Event_GameStart=function(e){
+            console.log(this.Identity,e);
+        } 
+
+        /**
+         * 事件 游戏回合
+         * 
+         * 在游戏轮到新的玩家下棋的时候本事件会被触发
+         * 
+         * 在判断完是轮到你下棋的时候即可调用 PlaceChess 方法下棋。
+         * 
+         * @param {Event} e 
+         */
+        this.Event_Round=function(e){
+            
+            console.log(this.Identity,e);
+
+            if (e.Operator==this) {
+                //轮到我下棋
+            } else {
+                //没有轮到我下棋
+            }
+
+        }
+
+        /**
+         * 事件 游戏结束
+         * 
+         * 在游戏结束的时候本事件会被触发
+         * 
+         * @param {Event} e 
+         */
+        this.Event_GameEnd=function(e){
+            console.log(this.Identity,e);
+        }
+
     }
 
 
-    
+
 }
