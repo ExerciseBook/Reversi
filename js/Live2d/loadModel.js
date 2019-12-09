a3200e7189994f2fccdfa32c8851ef0be1dd2195
Eric_Lian <git@SuperExerciseBook.com>
2019-12-09 19:09:38 +0800

    修个Bug

--- js/Live2d/loadModel.js
+++ js/Live2d/loadModel.js
@@ -1,7 +1,7 @@
 var LIVE2DCUBISMCORE = Live2DCubismCore
 //如果资源在CDN，一定要写http://或者https://否则会以本域名请求且为相对路径
 //模型的model3.json文件路径
-var baseModelPath = window.location.href + "/live2d/";
+var baseModelPath = "live2d/";
 var modelPath;
 //Application全局变量
 var app = null;
